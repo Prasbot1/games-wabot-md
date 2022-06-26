d467b92e27cfb5e312d429cc908b9d265fdefbb0
Prasbot1 <107738150+Prasbot1@users.noreply.github.com>
2022-06-26 09:58:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6289666747744'],
+  ['6289666747744'],
+  ['6289666747744', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = [6289666747744] // Want some help?
+global.prems = [6289666747744] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6289666747744'
+global.author = 'XyuziBot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
